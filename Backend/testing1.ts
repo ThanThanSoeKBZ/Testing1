e5b27cc86e3ce13df4ec515ff1b630aec306580f
thanthan.soe <thanthan.soe@kbzbank.com>
2025-10-19 02:30:24 +0630

    tts add testing1 file from develop branch

---
+++ Backend/testing1.ts
@@ -0,0 +1,10 @@
+function addNumbers1(a: number, b: number): number {
+    return a + b;
+}
+
+// Using the function
+const num3: number = 5;
+const num4: number = 10;
+
+const sum2: number = addNumbers1(num3, num4);
+console.log(`The sum of ${num3} and ${num4} is ${sum2}`);
