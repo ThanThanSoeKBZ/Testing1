f78401585dca86f79374fd3190bd9ccb413fcf77
thanthan.soe <thanthan.soe@kbzbank.com>
2025-10-19 02:38:38 +0630

    from develop

--- Backend/testing.ts
+++ Backend/testing.ts
@@ -1,4 +1,4 @@
-function addNumbers(a: number, b: number): number {
+function addNumber(a: number, b: number): number {
     return a + b;
 }
 
@@ -6,5 +6,5 @@ function addNumbers(a: number, b: number): number {
 const num1: number = 5;
 const num2: number = 10;
 
-const sum: number = addNumbers(num1, num2);
+const sum: number = addNumber(num1, num2);
 console.log(`The sum of ${num1} and ${num2} is ${sum}`);
